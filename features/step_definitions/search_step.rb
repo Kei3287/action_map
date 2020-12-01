d5c92020197cb0e346c443ea086991c13d195d6c
Keisuke Watanabe <kei3287@berkeley.edu>
2020-11-30 17:00:49 -0800

    add California civic result

--- features/step_definitions/search_step.rb
+++ features/step_definitions/search_step.rb
@@ -3,7 +3,7 @@
 When(/^(?:|I )submit "([^"]*)"$/) do |button|
     stub_request(:get, /.*civicinfo*/).to_return(
         headers: [['Content-Type', 'application/json; charset=UTF-8']],
-        body:    File.read(File.join('google_civic_api.json'))
+        body:    File.read(File.join('google_civic_api_CA.json'))
     )
     click_button(button)
 end
