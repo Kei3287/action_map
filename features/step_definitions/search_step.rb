44985acb3eb883b3045ec7f9ee2b6bda4ad82d4d
Keisuke Watanabe <kei3287@berkeley.edu>
2020-11-30 16:14:09 -0800

    fix format

--- features/step_definitions/search_step.rb
+++ features/step_definitions/search_step.rb
@@ -1,7 +1,9 @@
-When /^(?:|I )submit "([^"]*)"$/ do |button|
-  stub_request(:get, /.*civicinfo*/).to_return(
-    headers: [['Content-Type', 'application/json; charset=UTF-8']],
-    body:    File.read(File.join('google_civic_api.json'))
-  )
-  click_button(button)
+# frozen_string_literal: true
+
+When(/^(?:|I )submit "([^"]*)"$/) do |button|
+    stub_request(:get, /.*civicinfo*/).to_return(
+        headers: [['Content-Type', 'application/json; charset=UTF-8']],
+        body:    File.read(File.join('google_civic_api.json'))
+    )
+    click_button(button)
 end
